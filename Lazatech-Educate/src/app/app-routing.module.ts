c4a22a2cf12389be5f554a84742d2800c6e82816
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-23 11:12:10 +0800

    creating blank homepage

--- Lazatech-Educate/src/app/app-routing.module.ts
+++ Lazatech-Educate/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
 ];
 
 @NgModule({

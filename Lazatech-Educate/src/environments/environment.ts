19dceaab03cfd0b275396f2b75d0fc7705ba43d2
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-25 14:47:21 +0800

    adding firebase to the environment

--- Lazatech-Educate/src/environments/environment.ts
+++ Lazatech-Educate/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBB0KVI1ozGWEZ5gP0BepDhhYIoQQjNNAQ",
+    authDomain: "lazatech-educate.firebaseapp.com",
+    projectId: "lazatech-educate",
+    storageBucket: "lazatech-educate.appspot.com",
+    messagingSenderId: "970839004977",
+    appId: "1:970839004977:web:eca9579ab08f24a5414210"
+  }
 };
 
 /*

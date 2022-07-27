f6f0e11772393473d26f37cfdb20a9f9fd86d8f3
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-27 22:00:58 +0800

    test updating data from db for future use

--- Lazatech-Educate/src/app/pages/my-token-balance/my-token-balance.page.ts
+++ Lazatech-Educate/src/app/pages/my-token-balance/my-token-balance.page.ts
@@ -10,22 +10,32 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class MyTokenBalancePage implements OnInit {
 
+  userId: string;
   user: any;
-  nToken: number;
-  id: any;
+  name: string;
+  email: string;
+  token: number;
 
 
   constructor(private auth: AuthService, private afs: AngularFirestore, private afauth: AngularFireAuth) {}
 
   ngOnInit() {
     this.auth.user$.subscribe(user => {
+      this.userId = user.userId;
       this.user = user;
+      this.name = user.userName;
+      this.email = user.userEmail;
+      this.token = user.userToken;
     })
   }
 
-  // update(){
-  //   this.nToken = this.user.userToken += 1
-
-  // }
+  // for testing purposes only
+  add(){
+    this.afs.collection('user').doc(this.userId).set({
+      'userName': this.name,
+      'userEmail': this.email,
+      'userToken': this.token + 1
+    },{merge: true});
+  }
 
 }

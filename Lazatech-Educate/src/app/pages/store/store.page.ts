a7e8d30609491a8201d5338f99a12b70a4d80186
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-29 17:01:38 +0800

    I'm working on the store page

--- Lazatech-Educate/src/app/pages/store/store.page.ts
+++ Lazatech-Educate/src/app/pages/store/store.page.ts
@@ -8,6 +8,17 @@ import { Router } from '@angular/router';
 })
 export class StorePage implements OnInit {
 
+  links: any[] = [
+    '../../../assets/Lazatech-Merch/1.png',
+    '../../../assets/Lazatech-Merch/2.png',
+    '../../../assets/Lazatech-Merch/3.png',
+    '../../../assets/Lazatech-Merch/4.png',
+    '../../../assets/Lazatech-Merch/5.png',
+    '../../../assets/Lazatech-Merch/6.png',
+    '../../../assets/Lazatech-Merch/7.png',
+    '../../../assets/Lazatech-Merch/8.png'
+  ]
+
   id: any;
 
   constructor(private router: Router) { }

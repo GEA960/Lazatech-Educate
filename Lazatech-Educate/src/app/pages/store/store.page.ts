16bd5f24544735251ccf54a9beb953b1d9589968
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-30 15:15:21 +0800

    Store page is now functional

--- Lazatech-Educate/src/app/pages/store/store.page.ts
+++ Lazatech-Educate/src/app/pages/store/store.page.ts
@@ -30,7 +30,7 @@ export class StorePage implements OnInit {
 
   public open: boolean = false;
 
-  constructor(private router: Router, private afs: AngularFirestore, private auth: AuthService) { }
+  constructor(private router: Router, private afs: AngularFirestore, private auth: AuthService, private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.auth.user$.subscribe(user => {
@@ -42,19 +42,60 @@ export class StorePage implements OnInit {
     })
   }
 
-  // This is just a test
+  async alert(header, message, subheader, price){
+    const alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      subHeader: subheader,
+      buttons: [{
+        text: 'Cancel'
+        },
+        {
+          text: 'Buy',
+          handler: () =>{
+            this.pay(price)
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async alert2(header2, message2){
+    const alert2 = await this.alertCtrl.create({
+      header: header2,
+      message: message2,
+      buttons: ['Ok']
+  });
+  await alert2.present();
+}
+
   details(i){
     if(i == 0 || (i == 1)){
-      console.log('mug');
+      this.alert('Lazatech Mug', `Token Balance: ${this.token}.00`, 'Price: 3.00 tokens', 3)
     }
     else if(i == 2 || (i == 3)){
-      console.log('tshirt');
+      this.alert('Lazatech Shirt', `Token Balance: ${this.token}.00`, 'Price: 5.00 tokens', 5)
     }
     else if(i == 4 || (i == 5)){
-      console.log('tumbler');
+      this.alert('Lazatech Tumbler', `Token Balance: ${this.token}.00`, 'Price: 7.00 tokens', 7)
     }
     else if(i == 6 || (i == 7)){
-      console.log('tote bag');
+      this.alert('Lazatech Tote Bag', `Token Balance: ${this.token}.00`, 'Price: 2.00 tokens', 2)
+    }
+  }
+
+  pay(price){
+    if(this.token < price){
+      this.alert2('Alert', 'Not enough Tokens')
+    }
+    else{
+      this.afs.collection('user').doc(this.userId).set({
+        'userName': this.name,
+        'userEmail': this.email,
+        'userToken': this.token - price
+      },{merge: true});
+      this.alert2('Alert', 'Purchased Successfully')
     }
   }
 

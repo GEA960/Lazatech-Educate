ad763fdf201ca7018c23a07a77304644ce2d79ea
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-31 14:39:54 +0800

    some changes

--- Lazatech-Educate/src/app/pages/quiz/quiz.page.ts
+++ Lazatech-Educate/src/app/pages/quiz/quiz.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/services/auth.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-quiz',
@@ -11,12 +12,26 @@ export class QuizPage implements OnInit {
   qstn: any;
   questions: any;
   qAnswer: any;
-  index: number = 0;
+  public index: number = 0;
   show: boolean;
 
-  constructor() { }
+  userId: string;
+  user: any;
+  name: string;
+  email: string;
+  token: number;
+
+  constructor(private afs: AngularFirestore, private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      this.userId = user.userId;
+      this.user = user;
+      this.name = user.userName;
+      this.email = user.userEmail;
+      this.token = user.userToken;
+    }),
+
     fetch('./assets/data/questions.json').then(res => res.json())
     .then(json => {
       this.qstn = json['C1L1questions'];
@@ -25,17 +40,24 @@ export class QuizPage implements OnInit {
 
   take(){
     this.show = true;
-    this.answer();
+    this.answer()
   }
 
   answer(){
     this.questions = this.qstn[this.index];
     if (this.qAnswer == null){
       console.log('No answer')
+      console.log(this.index)
     }
-    else{
-      console.log('Selected Value:', this.qAnswer)
-      return this.index = this.index + 1
+    else if(this.qAnswer == this.questions.answer){
+      console.log('Correct');
+      console.log(this.index)
+      this.index++;
+    }
+    else if(this.qAnswer != this.questions.answer){
+      console.log('Wrong');
+      console.log(this.index);
+      this.index++;
     }
   }
 

1119b1cee4f351df1cc173893d9d97c297956621
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-08-03 17:59:43 +0800

    additional changes

--- Lazatech-Educate/src/app/components/components.module.ts
+++ Lazatech-Educate/src/app/components/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ErrMessageComponent } from "./err-message/err-message.component";
+import { IonicModule } from "@ionic/angular";
 
 @NgModule({
     declarations: [ErrMessageComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, IonicModule],
     exports: [ErrMessageComponent]
 })
 
